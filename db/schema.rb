60825c11ee391952e718e3658ee47fa5010830e8
github nariakiraHara <nariakra@gmail.com>
2018-02-19 23:21:01 +0900

    micropost作成 foreachの理解

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215154928) do
+ActiveRecord::Schema.define(version: 20180219124010) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
